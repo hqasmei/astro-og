8a3c206dd94c57a8a1822b6b4529b444002b2a48
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-01-23 19:04:41 -0800

    Testing og

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
-
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://astro-og.vercel.app/",
+  output: "server",
+  adapter: vercel(),
+  integrations: [react()],
+});
