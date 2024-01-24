14f4a555f5b12ea8bfe3dd7f1ac4c4b332b66426
Hosna Qasmei <hosna.qasmei@gmail.com>
2024-01-23 19:45:33 -0800

    Reverted

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-og.vercel.app/",
   output: "server",
   adapter: vercel(),
-  integrations: [react()],
+  integrations: [ react() ],
+   vite: {
+    optimizeDeps: {
+      exclude: ["@resvg/resvg-js"],
+    },
+  },
 });
